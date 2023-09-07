dd3bb0c7e5f8612534dbb72aa1293f81f6f92699
AirZED <regnalukpabio@gmail.com>
2023-09-07 05:34:21 +0100

    new repo

--- task-one/src/controllers/getter.ts
+++ task-one/src/controllers/getter.ts
@@ -3,15 +3,22 @@ import { RequestHandler } from "express";
 export const getter: RequestHandler = (req, res, next) => {
   try {
     const { slack_name, track } = req.query;
-    const current_day = new Date(Date.now()).getDay();
+    const current_day = new Date(Date.now()).toLocaleDateString("en-US", {
+      weekday: "long",
+    });
     const utc_time = new Date(Date.now()).toUTCString();
-    //   const github
+    const github_file_url = process.env.GITHUB_FILE || "";
+    const github__repo_url = process.env.GITHUB_URL || "";
+    const status_code = 200;
 
     res.status(200).json({
       slack_name,
       current_day,
       utc_time,
       track,
+      github_file_url,
+      github__repo_url,
+      status_code,
     });
   } catch (error) {
     res.status(500).json({ status: "failed", message: "Something went wrong" });
