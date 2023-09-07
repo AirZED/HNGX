9436462abc66b1a09cbcfa6cae2532671d7d06f6
Mfoniso_Ukpabio <68617262+AirZED@users.noreply.github.com>
2023-09-07 11:55:53 +0100

    Update getter.ts utc time format

--- task-one/src/controllers/getter.ts
+++ task-one/src/controllers/getter.ts
@@ -6,7 +6,7 @@ export const getter: RequestHandler = (req, res, next) => {
     const current_day = new Date(Date.now()).toLocaleDateString("en-US", {
       weekday: "long",
     });
-    const utc_time = new Date(Date.now()).toUTCString();
+    const utc_time = new Date(Date.now()).toISOString();
     const github_file_url = process.env.GITHUB_FILE || "";
     const github_repo_url = process.env.GITHUB_URL || "";
     const status_code = 200;
