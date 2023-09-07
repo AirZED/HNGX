d138a26ba79d28482feeb29704058679eb8ad375
Mfoniso_Ukpabio <68617262+AirZED@users.noreply.github.com>
2023-09-07 11:41:49 +0100

    Update route definition in app.ts

--- task-one/src/app.ts
+++ task-one/src/app.ts
@@ -3,6 +3,6 @@ import express from "express";
 const app = express();
 import getterRouter from "./routes/getterRoute";
 
-app.use("/api/v1/details", getterRouter);
+app.use("/api", getterRouter);
 
 export default app;
